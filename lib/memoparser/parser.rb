48df25bf867e1a26128733dd3b9daff2129f64f9
noam <clusterfoo@server.fake>
2013-07-31 16:15:38 -0400

    Parser module passing

--- lib/memoparser/parser.rb
+++ lib/memoparser/parser.rb
@@ -12,5 +12,32 @@ module MemoParser
     # example, a string for a ":user" label, an integer for a ":comment" label:
     #
     #   [[:relationship, :user, "joe_schmo"], [:relationship, :comment, 2456]]
+    
+    def self.parse_marker(array)
+      output = parse_node(array)
+      output = parse_label(array)
+      output = parse_identifier(array)
+      output
+    end
+
+    def self.parse_identifier(array)
+      # TODO: unuglify
+      if array[0] == "@"
+        array[0] = :relationships
+      elsif array[0] == "#"
+        array[0] = :tags
+      end
+      array
+    end
+
+    def self.parse_label(array)
+      array[1] = array[1].to_sym
+      array
+    end
+
+    def self.parse_node(array)
+      array[2] = array[2].to_sym
+      array
+    end
   end
 end
