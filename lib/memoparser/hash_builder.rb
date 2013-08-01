12efb6f7b5e8d2218d39f608e2bdf0da250a1865
noam <clusterfoo@server.fake>
2013-08-01 13:44:32 -0400

    HashBuilder module passing

--- lib/memoparser/hash_builder.rb
+++ lib/memoparser/hash_builder.rb
@@ -2,5 +2,34 @@ module MemoParser
   module HashBuilder
     # Given an input string, pass it through the tokenizer and parser and
     # produce a hash of the form seen in the README file.
+    def self.populate_label_arrays(array)
+      hash = build_label_arrays(array)
+      array.each do |marker|
+        identifier = marker[0]
+        label = marker[1]
+        node = marker[2]
+        hash[identifier][label] << node
+      end
+      hash
+    end
+
+    def self.build_label_arrays(array)
+      hash = build_identifier_hashes(array)  
+      array.each do |marker|
+        identifier = marker[0]
+        label = marker[1]
+        hash[identifier][label] = []
+      end
+      hash
+    end
+
+    def self.build_identifier_hashes(array)
+      hash = {} 
+      array.each do |marker|
+        identifier = marker[0]
+        hash[identifier] ||= {  }
+      end
+      hash
+    end
   end
 end
--- test/test_hash_builder.rb
+++ test/test_hash_builder.rb
@@ -3,32 +3,12 @@ require 'minitest/autorun'
 require_relative "helper"
 
 class TestTokenizer < Minitest::Test
-  def test_build_hash
-    input = "some string @some_marker, @ # #:not_marker 
-             @:not_marker @label1:node1 @label1:node1 
-             @label2:node @label2:node2 #label:node #node"
-    output = MemoParser::HashBuilder.build_hash(input)
-    expected_output = {
-                        relationships:  
-                        {
-                          memo: ["some_marker"],
-                          label1: ["node1"],
-                          label2: ["node", "node2"]
-                        },
-                        tags:
-                        {
-                          tag: ["node"],
-                          label: ["node"]
-                        }
-                      }
-    assert_equal expected_output, output
-  end
-
-  def test_build_identifier_hashes
+  def test_populate_label_arrays
     input = [[:relationships, :memo, "a27b"], [:relationships, :memo, "b27"],
              [:relationships, :update, "a27b"], [:tags, :tag, "poo"]]
-    output = MemoParser::HashBuilder.build_identifier_hashes(input)
-    expected_output = { relationships: {}, tags: {} }
+    output = MemoParser::HashBuilder.populate_label_arrays(input)
+    expected_output = { relationships: { memo: ["a27b", "b27"].sort, 
+                        update: ["a27b"] }, tags: { tag: ["poo"] } }
     assert_equal expected_output, output
   end
 
@@ -41,12 +21,11 @@ class TestTokenizer < Minitest::Test
     assert_equal expected_output, output
   end
 
-  def test_populate_label_arrays
+  def test_build_identifier_hashes
     input = [[:relationships, :memo, "a27b"], [:relationships, :memo, "b27"],
              [:relationships, :update, "a27b"], [:tags, :tag, "poo"]]
-    output = MemoParser::HashBuilder.build_label_arrays(input)
-    expected_output = { relationships: { memo: ["a27b", "b27"], 
-                        update: ["a27b"] }, tags: { tag: ["poo"] } }
+    output = MemoParser::HashBuilder.build_identifier_hashes(input)
+    expected_output = { relationships: {}, tags: {} }
     assert_equal expected_output, output
   end
 end
