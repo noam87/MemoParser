3f2aa36d67d05e2e2561acaf7eecf3b0ab204998
noam <clusterfoo@server.fake>
2013-07-31 20:39:34 -0400

    typo

--- test/test_hash_builder.rb
+++ test/test_hash_builder.rb
@@ -35,7 +35,7 @@ class TestTokenizer < Minitest::Test
   def test_build_label_arrays
     input = [[:relationships, :memo, "a27b"], [:relationships, :memo, "b27"],
              [:relationships, :update, "a27b"], [:tags, :tag, "poo"]]
-    output = MemoParser::HashBuilder.build_identifier_hashes(input)
+    output = MemoParser::HashBuilder.build_label_arrays(input)
     expected_output = { relationships: { memo: [], update: [] }, 
                         tags: { tag: [] } }
     assert_equal expected_output, output
@@ -44,7 +44,7 @@ class TestTokenizer < Minitest::Test
   def test_populate_label_arrays
     input = [[:relationships, :memo, "a27b"], [:relationships, :memo, "b27"],
              [:relationships, :update, "a27b"], [:tags, :tag, "poo"]]
-    output = MemoParser::HashBuilder.build_identifier_hashes(input)
+    output = MemoParser::HashBuilder.build_label_arrays(input)
     expected_output = { relationships: { memo: ["a27b", "b27"], 
                         update: ["a27b"] }, tags: { tag: ["poo"] } }
     assert_equal expected_output, output
